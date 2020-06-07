075fac6c05e6934bf8cf74a45895f14bd6935142
Karl Horky <karl.horky@gmail.com>
2020-06-07 12:44:40 +0200

    Format with Prettier@2

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const secured = require('../lib/middleware/secured');
 const router = express.Router();
 
 /* GET user profile. */
-router.get('/user', secured(), function(req, res, next) {
+router.get('/user', secured(), function (req, res, next) {
   const { _raw, _json, ...userProfile } = req.user;
   res.render('user', {
     userProfile: JSON.stringify(userProfile, null, 2),
